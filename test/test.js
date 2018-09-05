a4db16cafca4f8272cc2cc5704c2bd828ecf6d36
Kennedy Rose <kennedyianrose@gmail.com>
2018-09-05 16:48:44 -0500

    Test imports

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-import { onRouteUpdate } from '../gatsby-browser'
+import GatsbySSR from '../gatsby-ssr'
+import GatsbyNode from '../gatsby-node'
 
 describe('Gatsby plugins', () => {
 	it('should import', () => {
